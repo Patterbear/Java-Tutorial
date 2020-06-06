297bd6be0b6917f301a52117b5863b50d7631b00
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-06-06 18:49:57 +0100

    iterators

--- JavaHandsOn/src/MainClass.java
+++ JavaHandsOn/src/MainClass.java
@@ -2,47 +2,43 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class MainClass {
 
 	public static void main(String[] args) {
 		
-		//Creating exception for non-existent file which prints Stack trace
-				try {
-					Scanner in = new Scanner(new File("test"));
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-								
-		//Creation of Linked Lists
-				LinkedList<String> names = new LinkedList<String>();
-				LinkedList<String> animals = new LinkedList<String>();
-				
-		//Using Linked List as a Queue
-				
-		//Elements added to queue
-				names.add("Benjamin");
-				names.add("Adam");
-				names.add("Moss");
-				
-		//Elements 'dequeued' from queue and outputted
-				System.out.println(names.remove());
-				System.out.println(names.remove());
-				System.out.println(names.remove());
-				
-		//Using LinkedList as a Stack
-				
+		//Linked List Creation
+		LinkedList<String> names = new LinkedList<String>();
+		
 		//Elements 'pushed' onto stack
-				animals.push("Coyote");
-				animals.push("Buffalo");
-				animals.push("Eagle");
-				
-		//Elements 'popped' from stack and outputted
-				System.out.println(animals.pop());
-				System.out.println(animals.pop());
-				System.out.println(animals.pop());
+		names.push("Benjamin");
+		names.push("Adam");
+		names.push("Richard");
+		
+		//ListIterator object instantiated
+		ListIterator<String> it = names.listIterator();
+		
+		//Iterating through the list and adding an element
+		it.next();
+		it.next();
+		it.add("Chris");
+		
+		//While loop to output stack contents
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
+		//Element added to list at specified index
+		names.add(2, "Harry");
+		
+		//For Each loop to output stack contents
+		for(String s: names) {
+			System.out.println(s);
+		}
 		
 	}
 

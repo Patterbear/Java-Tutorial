b55840fa835d327d54c9341678f5cc6e13d0cf63
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-06-06 19:20:10 +0100

    working with 2d arrays

--- JavaHandsOn/src/MainClass.java
+++ JavaHandsOn/src/MainClass.java
@@ -1,45 +1,28 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Scanner;
-
 public class MainClass {
 
 	public static void main(String[] args) {
 		
-		//Linked List Creation
-		LinkedList<String> names = new LinkedList<String>();
+		//Creation  of 2D array
+		int[][] data = {
+				{4, 6, 3, 10},
+				{4, 2, 40, 1, 5, 3, 2, 30},
+				{5, 34, 1, 43}
+		};
 		
-		//Elements 'pushed' onto stack
-		names.push("Benjamin");
-		names.push("Adam");
-		names.push("Richard");
+		//Changing element of 2D array
+		data[1][2] = 49;
 		
-		//ListIterator object instantiated
-		ListIterator<String> it = names.listIterator();
-		
-		//Iterating through the list and adding an element
-		it.next();
-		it.next();
-		it.add("Chris");
-		
-		//While loop to output stack contents
-		while(it.hasNext()) {
-			System.out.println(it.next());
+		//Outputting specific element of 2D array
+		System.out.println(data[1][2]);
+	
+		//Outputting every element of a 2D array using a nested For loop
+		for(int i = 0; i < data.length; i++) {
+			for(int j =0; j < data[i].length; j++) {
+				System.out.print(data[i][j] + " ");
+			}
+			System.out.println();
 		}
-		
-		//Element added to list at specified index
-		names.add(2, "Harry");
-		
-		//For Each loop to output stack contents
-		for(String s: names) {
-			System.out.println(s);
-		}
-		
+	
 	}
 
 }

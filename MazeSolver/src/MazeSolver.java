d3ad4050906bfbe457c4654acee0bf5e64ff1a5b
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-06-11 16:55:06 +0100

    OOP refinement

--- MazeSolver/src/MazeSolver.java
+++ MazeSolver/src/MazeSolver.java
@@ -2,29 +2,38 @@ import java.util.LinkedList;
 
 public class MazeSolver {
 	
-	//Creation of maze as static collection
-	static int[][] maze = {
+	//Instantiation of class 'Maze' as object 'm'
+	static Maze m = new Maze();
+	
+	//Main Class
+	public static void main(String[] args) {
+		
+		//Maze created as 2D array
+		int[][] maze = {
 			{1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
 			{0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0},
 			{0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1},
 			{1, 1, 1, 2, 0, 1, 0, 1, 0, 1, 0},
 			{0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
 			{0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1}
-	};
-	
-	//Key
-	//0 = wall
-	//1 = path
-	//2 = destination
-	
-	//Static Stack created to store the path the solver has taken
-	static LinkedList<Position> path = new LinkedList<Position>();
-	
-	//Main Class
-	public static void main(String[] args) {
+		};
+		
+		//Key
+		//0 = wall
+		//1 = path
+		//2 = destination
+		
+		//Maze object assigned to 'maze'
+		m.maze = maze;
+		
+		//Starting position set
+		m.start = new Position(4, 8);
+		
+		//Linked list created to store path taken
+		m.path = new LinkedList<Position>();
 		
 		//SolveMaze method called with starting coordinates
-		if(solveMaze(new Position(4, 8))) {
+		if(solveMaze(m.start)) {
 			System.out.println("You won");
 		}else {
 			System.out.println("No path.");
@@ -33,29 +42,29 @@ public class MazeSolver {
 	
 	//Method to solve maze with given position
 	private static boolean solveMaze(Position p) {
-		path.push(p);
+		m.path.push(p);
 		
 		//While loop to search the maze
 		while(true) {
 			
 			//Assigning stack values to variables
-			int y = path.peek().y;
-			int x = path.peek().x;
+			int y = m.path.peek().y;
+			int x = m.path.peek().x;
 
 			//Marking visited positions with a 0
-			maze[y][x] = 0;
+			m.maze[y][x] = 0;
 			
 			//If statement to check whether a move down is possible
 			if(isValid(y+1, x)) {
 				
 				//Checking value below position
-				if(maze[y+1][x] == 2) {
+				if(m.maze[y+1][x] == 2) {
 					System.out.println("Moved down");
 					System.out.println("You won");
 					return true;
-				} else if(maze[y+1][x] == 1) {
+				} else if(m.maze[y+1][x] == 1) {
 					System.out.println("Moved down");
-					path.push(new Position(y+1, x));
+					m.path.push(new Position(y+1, x));
 					continue;
 				}
 			}
@@ -64,12 +73,12 @@ public class MazeSolver {
 			if(isValid(y, x-1)) {
 				
 				//Checking value left of position
-				if(maze[y][x-1] == 2) {
+				if(m.maze[y][x-1] == 2) {
 					System.out.println("Moved left");
 					return true;
-				} else if(maze[y][x-1] == 1) {
+				} else if(m.maze[y][x-1] == 1) {
 					System.out.println("Moved left");
-					path.push(new Position(y, x-1));
+					m.path.push(new Position(y, x-1));
 					continue;
 				}
 			}
@@ -78,12 +87,12 @@ public class MazeSolver {
 			if(isValid(y-1, x)) {
 				
 				//Checking value above position
-				if(maze[y-1][x] == 2) {
+				if(m.maze[y-1][x] == 2) {
 					System.out.println("Moved up");
 					return true;
-				} else if(maze[y-1][x] == 1) {
+				} else if(m.maze[y-1][x] == 1) {
 					System.out.println("Moved up");
-					path.push(new Position(y-1, x));
+					m.path.push(new Position(y-1, x));
 					continue;
 				}
 			}
@@ -92,22 +101,22 @@ public class MazeSolver {
 			if(isValid(y, x+1)) {
 				
 				//Checking value right of position
-				if(maze[y][x+1] == 2) {
+				if(m.maze[y][x+1] == 2) {
 					System.out.println("Moved left");
 					return true;
-				} else if(maze[y][x+1] == 1) {
+				} else if(m.maze[y][x+1] == 1) {
 					System.out.println("Moved left");
-					path.push(new Position(y, x+1));
+					m.path.push(new Position(y, x+1));
 					continue;
 				}
 			}
 			
 			//Popping latest coordinates from stack
-			path.pop();
+			m.path.pop();
 			System.out.println("Backtracking...");
 			
 			//Message output if stack is empty
-			if(path.size() <= 0) {
+			if(m.path.size() <= 0) {
 				return false;
 			}
 		}
@@ -116,9 +125,9 @@ public class MazeSolver {
 	//Method to check whether the current coordinate is out bounds
 	public static boolean isValid(int y, int x) {
 		if(y < 0 ||
-				y >= maze.length ||
+				y >= m.maze.length ||
 				x < 0 ||
-				x >= maze[y].length
+				x >= m.maze[y].length
 				) {
 			return false;
 		}

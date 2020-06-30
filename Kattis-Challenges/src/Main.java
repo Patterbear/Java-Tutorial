07f0cc6f25ce63b3686298008f0234882759a2fd
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-06-30 18:46:11 +0100

    Tarifa

--- Kattis-Challenges/src/Main.java
+++ Kattis-Challenges/src/Main.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Scanner in = new Scanner(System.in);
+		
+		int x = in.nextInt();
+		int n = in.nextInt();
+		int p = 0;
+		int r = 0;
+		
+		for(int i = 0; i < n; i++) {
+			p = in.nextInt();
+			r = r + x - p;
+		}
+		System.out.println((r + x));
+		
 	}
 
 }

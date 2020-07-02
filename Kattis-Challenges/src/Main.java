b69806e84bd806981f338d7de1c1eb8309dce8d4
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-07-02 18:41:11 +0100

    filip

--- Kattis-Challenges/src/Main.java
+++ Kattis-Challenges/src/Main.java
@@ -4,16 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		StringBuilder numbersReversed = new StringBuilder();
 		
-		int n = in.nextInt();
+		String numbers = in.nextLine();
+		
+		numbersReversed.append(numbers);
+		numbersReversed = numbersReversed.reverse();
+		
+		int a = Integer.parseInt(numbersReversed.substring(4));
+		int b = Integer.parseInt(numbersReversed.substring(0, 3));
+		
+		if(a > b) {
+			System.out.println(a);
+		} else {
+			System.out.println(b);
+		}
+		
+		
 		
 		in.close();
 		
-		if (n % 2 == 0) {
-			System.out.println("Bob");
-		} else {
-			System.out.println("Alice");
-		}
 	} 
 
 }

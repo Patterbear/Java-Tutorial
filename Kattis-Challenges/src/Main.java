e7e53697aa0829d5e090ec9c14c0c92f18a537ab
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-07-03 16:12:05 +0100

    timeloop

--- Kattis-Challenges/src/Main.java
+++ Kattis-Challenges/src/Main.java
@@ -4,26 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		StringBuilder numbersReversed = new StringBuilder();
 		
-		String numbers = in.nextLine();
-		
-		numbersReversed.append(numbers);
-		numbersReversed = numbersReversed.reverse();
-		
-		int a = Integer.parseInt(numbersReversed.substring(4));
-		int b = Integer.parseInt(numbersReversed.substring(0, 3));
-		
-		if(a > b) {
-			System.out.println(a);
-		} else {
-			System.out.println(b);
-		}
-		
-		
-		
+		int n = in.nextInt();
 		in.close();
 		
+		for(int i = 0; i < n; i++) {
+			System.out.println(i + 1 + " Abracadabra");
+		}
 	} 
 
 }

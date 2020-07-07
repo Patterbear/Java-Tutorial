f7a745169658bfc3339daeccb7c8afa09c33e75d
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-07-07 20:41:39 +0100

    bela

--- Kattis-Challenges/src/Main.java
+++ Kattis-Challenges/src/Main.java
@@ -6,49 +6,57 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Integer> mostLikely = new ArrayList<Integer>();
-		Map<Integer, Integer> dic = new HashMap<Integer, Integer>();
+		
+		HashMap<String, Integer> domValues = new HashMap<String, Integer>();
+		
+		domValues.put("A", 11);
+		domValues.put("K", 4);
+		domValues.put("Q", 3);
+		domValues.put("J", 20);
+		domValues.put("T", 10);
+		domValues.put("9", 14);
+		domValues.put("8", 0);
+		domValues.put("7", 0);
+		
+		HashMap<String, Integer> values = new HashMap<String, Integer>();
+		
+		values.put("A", 11);
+		values.put("K", 4);
+		values.put("Q", 3);
+		values.put("J", 2);
+		values.put("T", 10);
+		values.put("9", 0);
+		values.put("8", 0);
+		values.put("7", 0);
+		
 		Scanner in = new Scanner(System.in);
+		String nb = in.nextLine();
 		
-		String faces = in.nextLine();
-		in.close();
+		String[] nbArray = nb.split(" ", 2);
 		
-		String[] facesArray = faces.split(" ", 2);
-
-		int n = Integer.parseInt(facesArray[0]);
-		int m = Integer.parseInt(facesArray[1]);
-		int max = n + m;
+		int t = 0;
+		int n = Integer.parseInt(nbArray[0]);
+		String b = nbArray[1];
+		String c;
+		String v;
+		String s;
 		
-		for(int i = 0; max > 1; i++) {
-			dic.put(max, 0);
-			max = max - 1;
-		}
 		
-		for(int i = 1; i < n + 1; i++) {
-			for(int j = 1; j < m + 1; j++) {
-				int k = i + j;
-				dic.put(k, dic.get(k) + 1);
-			}
-		}
-		
-		mostLikely.add(0);
-		int o = 0;
-		
-		for(int i = 2; i < dic.size(); i ++) {
-			if (dic.get(i) > o) {
-				o = dic.get(i);
-				mostLikely.clear();
-				mostLikely.add(i);
-			}
-			else if(dic.get(i) == o) {
-				mostLikely.add(i);
-			}
+		for(int i = 0; i <  n * 4; i++) {
+			c = in.nextLine();
+			v = c.substring(0, 1);
+			s = c.substring(1);
 			
+			if (s.equals(b)) {
+				t = t + domValues.get(v);
+			}
+			else {
+				t = t + values.get(v);
+			}
 		}
+		System.out.println(t);
 		
-		for(int i = 0; i < mostLikely.size(); i++) {
-			System.out.println(mostLikely.get(i));
-		}
+		
 	}
 
 }

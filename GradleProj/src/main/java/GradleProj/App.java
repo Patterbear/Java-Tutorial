ac4caf766cce0fbc3ab9349b09a24dd704a82e31
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-07-22 18:58:31 +0100

    gradle setup commented

--- GradleProj/src/main/java/GradleProj/App.java
+++ GradleProj/src/main/java/GradleProj/App.java
@@ -1,9 +1,18 @@
 package GradleProj;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
 	public static void main(String[] args) {
+		
+		//Text is output
 		System.out.println("Gradle is operational");
+		
+		//Test List and ArrayList created
+		ArrayList<String> stuff = new ArrayList<String>();
+		List<Object> test;
 
 	}
 

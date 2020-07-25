b5ca9a866414bd1974ee6a99ad4d7fc1ebe73100
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-07-25 10:50:22 +0100

    layout manager

--- GUI_Demo/src/GUIDemo.java
+++ GUI_Demo/src/GUIDemo.java
@@ -1,26 +1,42 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
+import java.awt.Container;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
 public class GUIDemo {
 	
 	private JFrame frame;
-	private JButton button;
+	private JTextField input;
+	private JLabel label;
+	private JButton button1;
+	private JButton button2;
 	private int width;
 	private int height;
 	
 	public GUIDemo(int w, int h) {
 		frame = new JFrame();
-		button = new JButton("CLICK ME");
+		label = new JLabel("Hello");
+		input = new JTextField(10);
+		button1 = new JButton("Button 1");
+		button2 = new JButton("Button 2");
 		width = w;
 		height = h;
 	}
 	
 	public void setUpGUI() {
+		Container cp = frame.getContentPane();
+		FlowLayout flow = new FlowLayout();
+		cp.setLayout(flow);
 		frame.setSize(width, height);
 		frame.setTitle("GUI Demo");
-		frame.add(button);
+		cp.add(input);
+		cp.add(label);
+		cp.add(button1);
+		cp.add(button2);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
@@ -33,7 +49,7 @@ public class GUIDemo {
 			}
 		};
 		
-		button.addActionListener(buttonListener);
+		button1.addActionListener(buttonListener);
 	}
 
 }

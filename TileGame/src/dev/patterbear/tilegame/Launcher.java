94417ff09c47ee5a7f6a8243b729d7a1479c83d1
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-08-27 11:31:55 +0100

    window

--- TileGame/src/dev/patterbear/tilegame/Launcher.java
+++ TileGame/src/dev/patterbear/tilegame/Launcher.java
@@ -1,9 +1,11 @@
 package dev.patterbear.tilegame;
 
+import dev.patterbear.tilegame.display.Display;
+
 public class Launcher {
 	
 	public static void main(String[] args) {
-		
+		new Display("Game Title", 300, 300);
 	}
 
 }

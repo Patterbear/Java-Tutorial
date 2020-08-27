9afd1206e53f2b9ddceeac18ced7b3499febcfdd
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-08-27 14:08:04 +0100

    canvas

--- TileGame/src/dev/patterbear/tilegame/Launcher.java
+++ TileGame/src/dev/patterbear/tilegame/Launcher.java
@@ -5,7 +5,7 @@ import dev.patterbear.tilegame.display.Display;
 public class Launcher {
 	
 	public static void main(String[] args) {
-		new Display("Game Title", 300, 300);
+		new Game("Game Name", 400, 400);
 	}
 
 }
--- TileGame/src/dev/patterbear/tilegame/display/Display.java
+++ TileGame/src/dev/patterbear/tilegame/display/Display.java
@@ -1,10 +1,14 @@
 package dev.patterbear.tilegame.display;
 
+import java.awt.Canvas;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Display {
 	
 	private JFrame frame;
+	private Canvas canvas;
 	
 	private String title;
 	private int width, height;
@@ -25,6 +29,14 @@ public class Display {
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+		
+		canvas = new Canvas();
+		canvas.setPreferredSize(new Dimension(width, height));
+		canvas.setMaximumSize(new Dimension(width, height));
+		canvas.setMinimumSize(new Dimension(width, height));
+		
+		frame.add(canvas);
+		frame.pack();
 	}
 
 }

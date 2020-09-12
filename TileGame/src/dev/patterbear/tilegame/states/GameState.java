0cbf14cfbe69f7f1c515e7392f60a9f479e02de9
Benjamin McGregor <benjamin.mcgregor2002@gmail.com>
2020-09-12 12:34:01 +0100

    starting worlds

--- TileGame/src/dev/patterbear/tilegame/states/GameState.java
+++ TileGame/src/dev/patterbear/tilegame/states/GameState.java
@@ -6,29 +6,31 @@ import dev.patterbear.tilegame.Game;
 import dev.patterbear.tilegame.entities.creatures.Player;
 import dev.patterbear.tilegame.gfx.Assets;
 import dev.patterbear.tilegame.tiles.Tile;
+import dev.patterbear.tilegame.worlds.World;
 
 public class GameState extends State {
 	
 	private Player player;
+	private World world;
 	
 
 	public GameState(Game game) {
 		super(game);
 		player = new Player(game, 100, 100);
+		world = new World("");
 	}
 	
 	@Override
 	public void tick() {
+		world.tick();
 		player.tick();
 		
 	}
 
 	@Override
 	public void render(Graphics g) {
+		world.render(g);
 		player.render(g);
-		Tile.tiles[0].render(g, 0, 0);
-		Tile.tiles[1].render(g, 0, 70);
-		Tile.tiles[2].render(g, 0, 140);
 	}
 
 }
